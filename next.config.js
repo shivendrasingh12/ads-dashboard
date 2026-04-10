da2aacf06ee7a25e10701e3465e888ab8086e04d
shivendra singh <shivendrasingh@shivendras-MacBook-Air.local>
2026-04-11 02:04:30 +0530

    Bust Vercel build cache

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_APP_NAME: 'Ads Command',
-  },
+  generateBuildId: async () => '1775853270',
 }
-
 module.exports = nextConfig
